2265666fe3041d15a6bf6152142f68fcfd945be2
André Schnabel <aschnabel@gams.com>
2022-08-26 09:59:12 +0200

    Added GDX signature to GDX to YAML script

--- gdxtoyaml/main.py
+++ gdxtoyaml/main.py
@@ -1,19 +1,46 @@
 import struct
 import yaml
 
+
 def read_gdx(fn):
     with open(fn, 'rb') as fp:
-        header = {}
-        def read_byte(): return ord(fp.read(1))
+        def read_byte():
+            return ord(fp.read(1))
+
+        def read_string():
+            len = ord(fp.read(1))
+            return str(fp.read(len), 'ascii')
+
+        header = dict()
         header['size_of_word'] = read_byte()
-        header['byteorder'] = endian = 'little' if hex(int.from_bytes(fp.read(2), byteorder='little')) == '0x1234' else 'big'
+        islittle = hex(int.from_bytes(fp.read(2), byteorder='little')) == '0x1234'
+        header['byteorder'] = endian = 'little' if islittle else 'big'
         header['sizeof_int'] = intsize = read_byte()
-        def read_int(): return int.from_bytes(fp.read(intsize), byteorder=endian)
+
+        def read_int():
+            return int.from_bytes(fp.read(intsize), byteorder=endian)
+
         header['example_int'] = hex(read_int())
         header['sizeof_double'] = dblsize = read_byte()
-        def read_dbl(): return struct.unpack('d', fp.read(dblsize))[0]
+
+        def read_dbl():
+            return struct.unpack('d', fp.read(dblsize))[0]
+
         header['example_double'] = read_dbl()
-        obj = dict(header=header)
+
+        gdx_sig = {}
+        gdx_sig['gdx_header_nr'] = read_byte()
+        gdx_sig['gdx_header_id'] = read_string()
+        gdx_sig['version'] = read_int()
+        gdx_sig['compressed'] = read_int()
+
+        def trunc(s, maxlen):
+            return s if len(s) <= maxlen else s[:maxlen] + '...'
+
+        gdx_sig['audit_line'] = trunc(read_string(), 40)
+        gdx_sig['producer'] = read_string()
+
+        obj = dict(header=header, gdx_signature=gdx_sig)
         print(yaml.dump(obj))
 
 
