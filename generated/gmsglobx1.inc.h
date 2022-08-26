0ea290ec9beda1fdd2d8c6708192c34d8ccdad79
André Schnabel <aschnabel@gams.com>
2022-08-26 16:27:00 +0200

    Forgot to add two generated files

---
+++ generated/gmsglobx1.inc.h
@@ -0,0 +1,9 @@
+const int
+    maxPlatforms = 4,
+    maxSetConstants = 23,
+    maxSolverNames = 137,
+    maxModelTypesX = 16,
+    maxSolverTypePlatformMap = 1182,
+    maxSolverPlatformMap = 374,
+    maxClipCodes = 67,
+    maxTLLicense = 6;
