bc38c390837100d9c1132efa5f8f9db43b945b5a
André Schnabel <aschnabel@gams.com>
2022-09-05 10:50:17 +0200

    Refactored GDX interface tests

--- tests/gdxinterfacetests.cpp
+++ tests/gdxinterfacetests.cpp
@@ -9,63 +9,80 @@ using namespace std::literals::string_literals;
 namespace tests::gdxinterfacetests {
     TEST_SUITE_BEGIN("GDX interface tests");
 
-    void writeFileWithSingleUEL(gdxinterface::GDXInterface &pgx, const std::string &fn, int &ErrNr) {
-        pgx.gdxOpenWrite(fn, "xptests", ErrNr);
-        pgx.gdxUELRegisterRawStart();
-        pgx.gdxUELRegisterRaw("New-York");
-        pgx.gdxUELRegisterDone();
-        pgx.gdxClose();
-    }
-
-    void readFileWithSingleUEL(gdxinterface::GDXInterface &pgx, const std::string &fn, int &ErrNr) {
-        pgx.gdxOpenRead(fn, ErrNr);
-        int uelCnt, highMap, uelMap;
-        std::string uel;
-        pgx.gdxUMUelInfo(uelCnt, highMap);
-        REQUIRE_EQ(0, highMap);
-        REQUIRE_EQ(1, uelCnt);
-        pgx.gdxUMUelGet(1, uel, uelMap);
-        REQUIRE_EQ("New-York", uel);
-        REQUIRE_EQ(-1, uelMap);
-        pgx.gdxClose();
-    }
-
-    enum UELTestOps {
-        read = 0x01,
-        write = 0x10,
-        readAndWrite = 0x11
-    };
-
-    template<typename T> void runAddingUELTest(const std::string &fn, UELTestOps ops, bool cleanup=false) {
-        int ErrNr;
-        std::string ErrMsg;
-        if(ops & write)
+    template<typename T>
+    void testWriteOp(const std::string &fn,
+                     const std::function<void(gdxinterface::GDXInterface&)> &cb,
+                     bool cleanup = false) {
         {
-            auto pgxObj {std::make_unique<T>(ErrMsg)};
-            writeFileWithSingleUEL(*pgxObj, fn, ErrNr);
-        }
-        if(ops & read)
-        {
-            auto pgxObj {std::make_unique<T>(ErrMsg)};
-            readFileWithSingleUEL(*pgxObj, fn, ErrNr);
+            std::string ErrMsg;
+            T pgx{ErrMsg};
+            int ErrNr;
+            pgx.gdxOpenWrite(fn, "xpwraptest", ErrNr);
+            cb(pgx);
+            pgx.gdxClose();
         }
         if(cleanup)
             std::filesystem::remove(fn);
-    };
+    }
 
-    TEST_CASE("Test adding uels") {
-        std::array filenames{ "uel_wrapper.gdx"s, "uel_port.gdx"s };
-        runAddingUELTest<xpwrap::GDXFile>(filenames.front(), write);
-        runAddingUELTest<gxfile::TGXFileObj>(filenames.back(), write);
-        auto maybeMismatches = utils::binaryFileDiff(filenames.front(), filenames.back());
+    template<typename T>
+    void testReadOp(const std::string &fn,
+                    const std::function<void(gdxinterface::GDXInterface&)> &cb,
+                    bool cleanup = false) {
+        {
+            std::string ErrMsg;
+            T pgx{ErrMsg};
+            int ErrNr;
+            pgx.gdxOpenRead(fn, ErrNr);
+            cb(pgx);
+            pgx.gdxClose();
+        }
+        if(cleanup)
+            std::filesystem::remove(fn);
+    }
+
+    void testReads(const std::string &filename1,
+                   const std::string &filename2,
+                   const std::function<void(gdxinterface::GDXInterface&)> &cb) {
+        testReadOp<xpwrap::GDXFile>(filename1, cb);
+        testReadOp<gxfile::TGXFileObj>(filename2, cb);
+    }
+
+    void testMatchingWrites(const std::string &filename1,
+                            const std::string &filename2,
+                            const std::function<void(gdxinterface::GDXInterface&)> &cb) {
+        testWriteOp<xpwrap::GDXFile>(filename1, cb);
+        testWriteOp<gxfile::TGXFileObj>(filename2, cb);
+        auto maybeMismatches = utils::binaryFileDiff(filename1, filename2);
         if (maybeMismatches) {
             for (const auto& mm : *maybeMismatches)
-                std::cout << "Mismatch at offset " << mm.offset << " with left hand side = " << mm.lhs << " and right hand side = " << mm.rhs << "\n";
+                std::cout   << "Mismatch at offset " << mm.offset << " with "
+                            << filename1 << " = " << mm.lhs << " and "
+                            << filename2 <<"= " << mm.rhs << "\n";
             std::cout << std::endl;
         }
         else std::cout << "No mismatches found!" << std::endl;
-        runAddingUELTest<xpwrap::GDXFile>(filenames.front(), read, true);
-        runAddingUELTest<gxfile::TGXFileObj>(filenames.back(), read, true);
+        REQUIRE_FALSE(maybeMismatches);
+    }
+
+    TEST_CASE("Test adding uels") {
+        std::array filenames{ "uel_wrapper.gdx"s, "uel_port.gdx"s };
+        auto [f1, f2] = filenames;
+        testMatchingWrites(f1, f2, [](gdxinterface::GDXInterface &pgx) {
+            pgx.gdxUELRegisterRawStart();
+            pgx.gdxUELRegisterRaw("New-York");
+            pgx.gdxUELRegisterDone();
+        });
+        testReads(f1, f2, [](gdxinterface::GDXInterface &pgx) {
+            int uelCnt, highMap, uelMap;
+            std::string uel;
+            pgx.gdxUMUelInfo(uelCnt, highMap);
+            REQUIRE_EQ(0, highMap);
+            REQUIRE_EQ(1, uelCnt);
+            pgx.gdxUMUelGet(1, uel, uelMap);
+            REQUIRE_EQ("New-York", uel);
+            REQUIRE_EQ(-1, uelMap);
+        });
     }
 
     TEST_SUITE_END();
