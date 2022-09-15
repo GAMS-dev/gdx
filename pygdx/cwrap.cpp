7521d4ba0bc3bb63d15c97d7468941a081f77778
André Schnabel <aschnabel@gams.com>
2022-09-15 13:48:05 +0200

    Tinkering with unicode

--- pygdx/cwrap.cpp
+++ pygdx/cwrap.cpp
@@ -1,19 +1,26 @@
 #include "cwrap.h"
 #include "../gxfile.h"
+#include <iostream>
 
 extern "C" int create_gdx_file(const char *filename) {
     std::string ErrMsg;
     gxfile::TGXFileObj gdx {ErrMsg};
     if(!ErrMsg.empty()) {
-        //std::cout << "Unable to create GDX object. Error message:\n" << ErrMsg << std::endl;
+        std::cout << "Unable to create GDX object. Error message:\n" << ErrMsg << std::endl;
         return 1;
     }
     int ErrNr;
     if(!gdx.gdxOpenWrite(filename, "cwrap", ErrNr)) {
-        //std::cout << "Error opening " << filename << " for writing. Error code = " << ErrNr << std::endl;
+        std::cout << "Error opening " << filename << " for writing. Error code = " << ErrNr << std::endl;
         return 1;
     }
     gdx.gdxDataWriteStrStart("i", "A simple set", 1, global::gmsspecs::dt_set, 0);
+    gxdefs::TgdxValues vals {};
+    gxdefs::TgdxStrIndex keys {};
+    for(int i{1}; i<=5; i++) {
+        keys[0] = "uel_" + std::to_string(i);
+        gdx.gdxDataWriteStr(keys, vals);
+    }
     gdx.gdxDataWriteDone();
     gdx.gdxClose();
     return 0;
---
+++ pygdx/unicode.py
@@ -0,0 +1,2 @@
+with open('emojis.txt', encoding='utf-8') as fp:
+    print(fp.read())
