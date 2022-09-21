496bb55d38daf1b8d64a3486c3650659026b0d6d
André Schnabel <aschnabel@gams.com>
2022-09-21 17:12:59 +0200

    Very simple tests for serialization

---
+++ tests/serializetests.cpp
@@ -0,0 +1,51 @@
+#include <filesystem>
+#include "doctest.h"
+#include "../gxfile.h"
+
+using namespace std::literals::string_literals;
+using namespace gxfile;
+
+namespace tests::serializetests {
+    TEST_SUITE_BEGIN("Test serialization of GDX to write operations and YAML");
+
+    enum class TestType {
+        TEXT,
+        YAML
+    };
+
+    static const auto
+        gdx_fn {"serialize_test.gdx"s},
+        yaml_fn {"serialize_test.yaml"s},
+        text_fn {"serialize_test.txt"s};
+
+    void commonFileCreation(TestType t) {
+        std::string msg;
+        TGXFileObj obj {msg};
+        obj.SetWriteModes(t == TestType::YAML, t == TestType::TEXT);
+        int errNr;
+        obj.gdxOpenWrite(gdx_fn, "serializetests", errNr);
+        obj.gdxDataWriteStrStart("i", "expl", 1, global::gmsspecs::dt_set, 0);
+        gxdefs::TgdxStrIndex keyStrs {};
+        gxdefs::TgdxValues vals {};
+        for(int i{}; i<5; i++) {
+            keyStrs.front() = "i"s+std::to_string(i+1);
+            obj.gdxDataWriteStr(keyStrs, vals);
+        }
+        obj.gdxDataWriteDone();
+        obj.gdxClose();
+    }
+
+    TEST_CASE("Write write ops as text") {
+        commonFileCreation(TestType::TEXT);
+        REQUIRE(std::filesystem::is_regular_file(text_fn));
+        std::filesystem::remove(gdx_fn);
+    }
+
+    TEST_CASE("YAML representation of GDX file data") {
+        commonFileCreation(TestType::YAML);
+        REQUIRE(std::filesystem::is_regular_file(yaml_fn));
+        std::filesystem::remove(gdx_fn);
+    }
+
+    TEST_SUITE_END();
+}
