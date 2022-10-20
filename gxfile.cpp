53d2a2478867076ece0949603222ade8c86e7e39
André Schnabel <aschnabel@gams.com>
2022-10-20 17:12:32 +0200

    Revert to incorrect gdxSymbolGetDomainX, since the corrected version is even worse

--- gxfile.cpp
+++ gxfile.cpp
@@ -2525,12 +2525,18 @@ namespace gxfile {
                 const std::string &S { DomainIDs[D] };
                 if (S.empty() || S == "*" || !IsGoodIdent(S)) (*SyPtr->SDomStrings)[D] = 0;
                 else {
-                    int ix = utils::indexOf(DomainStrList, S);
+                    // FIXME: Handling is not correct here, but the fixed version causes even bigger issues!
+                    /*int ix = utils::indexOf(DomainStrList, S);
                     if(ix == -1) {
                         DomainStrList.push_back(S);
                         ix = (int)DomainStrList.size() - 1;
                     }
-                    (*SyPtr->SDomStrings)[D] = ix + 1;
+                    (*SyPtr->SDomStrings)[D] = ix + 1;*/
+                    (*SyPtr->SDomStrings)[D] = utils::indexOf(DomainStrList, S);
+                    if ((*SyPtr->SDomStrings)[D] <= -1) {
+                        DomainStrList.push_back(S);
+                        (*SyPtr->SDomStrings)[D] = (int) DomainStrList.size();
+                    }
                 }
             }
         }
