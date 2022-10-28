594a7faceba883d6e90c247e7d6b2592c1d3b5a6
André Schnabel <aschnabel@gams.com>
2022-10-28 17:37:56 +0200

    Towards getting the TRunner working

--- gdlib/runner.cpp
+++ gdlib/runner.cpp
@@ -2,5 +2,31 @@
 
 namespace gdlib::runner {
 
+    TMsgHandler::TMsgHandler(const std::string &MsgPfx) {
 
+    }
+
+    void TMsgHandler::ErrorMessage(int ec, const std::string &s) {
+
+    }
+
+    void TMsgHandler::LogMessage(const std::string &s) {
+
+    }
+
+    void TMsgHandler::DebugMessage(const std::string &s) {
+
+    }
+
+    TRunner::TRunner() : FMsgHandler{"Runner"} {
+
+    }
+
+    void TRunner::ParamsAdd(const std::string &v) {}
+
+    void TRunner::SetExecutable(const std::string &v) {}
+
+    int TRunner::StartAndWait() { return 0; }
+
+    int TRunner::GetProgRC() const { return 0; }
 }
--- gdlib/runner.h
+++ gdlib/runner.h
@@ -1,19 +1,51 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 namespace gdlib::runner {
 
-    class TRunner {
+    const int EC_Cannot_modify = 1;
+    const int EC_Process_Active = 2;
+    const int EC_Empty_CMD_Line = 3;
+
+    enum TVisible {
+        vis_hide, vis_minimized, vis_normal
+    };
+
+    class TMsgHandler {
+        int FVerbose;
+        std::string FMsgPfx;
+
     public:
-        TRunner() = default;
+        TMsgHandler(const std::string &MsgPfx);
+        void ErrorMessage(int ec, const std::string &s);
+        void LogMessage(const std::string &s);
+        void DebugMessage(const std::string &s);
+    };
+
+
+
+    class TRunner {
+        TMsgHandler FMsgHandler;
+        std::string FExecutable;
+        std::vector<std::string> FParams;
+        std::string FWorkDir;
+        std::string FCommandLine;
+        bool FIsRunning, FInheritHandles, FUseShell;
+        TVisible FVisible;
+        int FProgRC;
+
+    public:
+        explicit TRunner();
+
         ~TRunner() = default;
 
-        void ParamsAdd(const std::string &v) {}
-        void SetExecutable(const std::string &v) {}
-        int StartAndWait() { return 0; }
+        void ParamsAdd(const std::string &v);
+        void SetExecutable(const std::string &v);
+        int StartAndWait();
 
-        int GetProgRC() const { return 0; }
+        int GetProgRC() const;
     };
 
 }
