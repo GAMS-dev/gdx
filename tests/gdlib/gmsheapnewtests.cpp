8c8ef2e03a1147f185aa8980e3d5ed9559860b06
André Schnabel <aschnabel@gams.com>
2023-02-13 12:17:16 +0100

    Expanded wrapped vs. ported GDX object tests to run batch benchmarks over all suites

--- tests/gdlib/gmsheapnewtests.cpp
+++ tests/gdlib/gmsheapnewtests.cpp
@@ -22,13 +22,13 @@ namespace tests::gmsheapnew {
         std::array<uint8_t, 20> buf2{};
         int64_t *ref {(int64_t*)buf2.data()};
         for(int i{}; i<n; i++) {
-            ptrs[i] = thm.XGetMem(buf2.size());
+            ptrs[i] = thm.XGetMem((int)buf2.size());
             *ref = (int64_t)i;
             std::memcpy(ptrs[i], buf2.data(), buf2.size());
         }
         for(int i{}; i<n; i++) {
             REQUIRE_EQ(*((int64_t*)ptrs[i]), (int64_t)i);
-            thm.XFreeMem(ptrs[i], buf2.size());
+            thm.XFreeMem(ptrs[i], (int)buf2.size());
         }
     }
 
--- tests/gxfiletests.cpp
+++ tests/gxfiletests.cpp
@@ -158,16 +158,49 @@ namespace tests::gxfiletests {
 
     void runBenchmarkTimeMemForGDXFile(const std::string &fn);
 
+    enum class BatchBenchModes {
+        SUITE_MODEL_PAIRS,
+        ALL_SUITES,
+        SINGLE_SUITE
+    };
+
+    void batchRunBenchmarks(BatchBenchModes mode);
+
     TEST_CASE("Run pfgdx for suiteName/modelName.gdx in order to debug memory issues (and test pfgdx port)") {
 #ifdef NO_SLOW_TESTS
         return;
 #endif
-        std::vector<std::pair<std::string, std::string>> suiteModelPairs {
+        batchRunBenchmarks(BatchBenchModes::SUITE_MODEL_PAIRS);
+    }
+
+    static const std::string benchOutFileName{ "gdxbench_collected.txt"s };
+
+    std::list<std::string> gdxFilesInPath(const std::string& path) {
+        std::list<std::string> res{};
+        for (const auto& entry : std::filesystem::directory_iterator{ path }) {
+            const auto fn = entry.path().filename().string();
+            if (utils::ends_with(fn, ".gdx")) {
+                auto entry = fn.substr(0, fn.size() - 4);
+                res.push_back(entry);
+            }
+        }
+        return res;
+    }
+
+    void batchRunBenchmarks(BatchBenchModes mode) {
+        if(std::filesystem::exists(benchOutFileName))
+            std::filesystem::remove(benchOutFileName);
+
+        std::array<std::string, 4> suites{
+            "sqagams"s, "lwsup"s, "src"s, "mrb"s
+        };
+
+        std::vector<std::pair<std::string, std::string>> suiteModelPairs{
 //            {"lwsup","modBig"},
 //            {"lwsup","modBigx"},
 //            {"lwsup","t4716"},
 //            {"sqagams","dummy42"},
-            {"lwsup","10rr"},
+              {"lwsup","10rr"},
 //            {"sqagams","t3010"},
 //            {"mrb","WSC Fixing"},
 //            {"lwsup","modBig8788"},
@@ -183,12 +216,14 @@ namespace tests::gxfiletests {
 //            {"sqagams","bearing"}
         };
 
-        auto gdxFilePathCandidates = [](const std::string &suiteName, const std::string &modelName) {
+        std::string singleSuiteName{ "src"s };
+
+        auto gdxFilePathCandidates = [](const std::string& suiteName, const std::string& modelName) {
 #if defined(_WIN32)
-            return std::array {
+            return std::array{
                 modelName + ".gdx"s,
                 suiteName + "\\"s + modelName + ".gdx"s,
-                R"(C:\dockerhome\)"+suiteName+"\\"s+modelName+".gdx"s
+                R"(C:\dockerhome\)" + suiteName + "\\"s + modelName + ".gdx"s
             };
 #else
             return std::array{
@@ -200,13 +235,103 @@ namespace tests::gxfiletests {
 #endif
         };
 
-        for(const auto &[suiteName, modelName] : suiteModelPairs)
-            for(const auto &fn : gdxFilePathCandidates(suiteName, modelName))
+        auto runBenchmarkForSuiteModel = [&](const std::string& suiteName, const std::string& modelName) {
+            for (const auto& fn : gdxFilePathCandidates(suiteName, modelName))
                 if (std::filesystem::exists(fn))
                     runBenchmarkTimeMemForGDXFile(fn);
+        };
+
+        std::string suitePrefixPath;
+#if defined(_WIN32)
+        suitePrefixPath = R"(C:\dockerhome\)";
+#else
+        suitePrefixPath = "/mnt/c/dockerhome/";
+#endif
+
+        switch (mode) {
+        case BatchBenchModes::ALL_SUITES:
+            for (const auto& suite : suites)
+                for (const auto& modelName : gdxFilesInPath(suitePrefixPath + suite))
+                    runBenchmarkForSuiteModel(suite, modelName);
+            break;
+        case BatchBenchModes::SINGLE_SUITE:
+            for (const auto& modelName : gdxFilesInPath(suitePrefixPath + singleSuiteName))
+                runBenchmarkForSuiteModel(singleSuiteName, modelName);
+            break;
+        case BatchBenchModes::SUITE_MODEL_PAIRS:
+            for (const auto& [suiteName, modelName] : suiteModelPairs)
+                runBenchmarkForSuiteModel(suiteName, modelName);
+            break;
+        }
     }
 
+    std::vector<std::string> skiplist {
+        "5062.gdx"s,
+        "data1000.gdx"s,
+        "data10000.gdx"s,
+        "Modellendogen0004.gdx"s,
+        "Modellendogen0005.gdx"s,
+        "Modellendogen0725.gdx"s,
+        "Modellendogen0726.gdx"s,
+        "Modellendogen1093.gdx"s,
+        "Modellendogen1407.gdx"s,
+        "output2.gdx"s,
+        "output_DisEmp.gdx"s,
+        "preFix.gdx"s,
+        "soln_Toroptimierung_2_p1.gdx"s,
+        "Toroptimierung_2_p.gdx"s,
+        "Toroptimierung_2_p - Copy.gdx"s,
+        "z.gdx"s,
+        "data_m5.gdx"s,
+        "dbX2370.gdx"s, "dbX2372.gdx"s,
+        "gmsgrid.gdx"s,
+        "testInstance1000Ite.gdx"s,
+        "testInstance100Ite.gdx"s,
+        "testInstance200Ite.gdx"s,
+        "x0.gdx"s,
+        "x1.gdx"s,
+        "x170.gdx"s,
+        "x170postfix.gdx"s,
+        "x171.gdx"s,
+        "test.gdx"s,
+        "gdxdump.gdx"s,
+        "map.gdx"s,
+        "new4.gdx"s,
+        "food_man.gdx"s,
+        "testExcel.gdx"s,
+        "ELMOD.gdx"s,
+        "JMM.gdx"s,
+        "arauco.gdx"s,
+        "modelo.gdx"s,
+        "PERSEUS.gdx"
+        "AssignmentBug.gdx"s,
+        "globiom.gdx"s,
+        "indata.gdx"s,
+        "seders.gdx"s,
+        "CHPSystemData2_Converted.gdx"s,
+        "pegase.gdx"s,
+        "getdata.gdx"s,
+        "rank_out.gdx"s,
+        "rank_output.gdx"s,
+        "bau_p.gdx"
+        "validnlpecopts.gdx"s,
+        "xyz.gdx"
+    };
+
     void runBenchmarkTimeMemForGDXFile(const std::string &fn) {
+#if defined(_WIN32)
+        const char sep { '\\' };
+#else
+        const char sep{ '/' };
+#endif
+        auto fnParts = utils::split(fn, sep);
+        auto filenamePart = fnParts.back();
+        auto filenameStem = filenamePart.substr(0, filenamePart.size() - 4);
+
+        if (std::any_of(skiplist.begin(), skiplist.end(),
+            [&](const std::string& s) { return s == filenamePart; }))
+            return;
+
         const int ntries = 1;
         const bool  quiet = false,
                 onlyPorted = false,
@@ -215,6 +340,8 @@ namespace tests::gxfiletests {
         pfgdx::TimeTriple tWrap, tPort;
         std::array<double, ntries> slowdowns {};
         double totWrap{}, totPort{};
+
+        std::ofstream textout {benchOutFileName, std::ios_base::app};
 
         for (int i = 0; i < ntries; i++) {
             int64_t peakRSS;
@@ -244,9 +371,11 @@ namespace tests::gxfiletests {
                 std::cout << "Slowdown for " << fn << " = " << slowdowns[i] << std::endl;
         }
         if (!quiet) {
-            std::cout << "Average slowdown = " << std::accumulate(slowdowns.begin(), slowdowns.end(), 0.0) / (double)ntries << std::endl;
+            auto avgSlowdown = std::accumulate(slowdowns.begin(), slowdowns.end(), 0.0) / (double)ntries;
+            std::cout << "Average slowdown = " << avgSlowdown << std::endl;
             std::cout << "Total runtime wrapped = " << totWrap << std::endl;
             std::cout << "Total runtime ported = " << totPort << std::endl;
+            textout << filenameStem << ";" << avgSlowdown << std::endl;
         }
     }
 
