55c2be6ad153110d19a81a6dc8ecebf49adb9e9c
André Schnabel <aschnabel@gams.com>
2023-03-20 19:04:41 +0100

    Fix wei build

--- src/tests/utilstests.cpp
+++ src/tests/utilstests.cpp
@@ -30,6 +30,7 @@
 #include <cstring>   // for strcmp
 #include <memory>    // for unique_ptr
 #include <string>// for operator""s, string, basic_string, allocator
+#include <ostream>
 
 using namespace std::literals::string_literals;
 
