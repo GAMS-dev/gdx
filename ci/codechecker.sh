6cd609db8624607781539b90af0be77a977de96b
André Schnabel <aschnabel@gams.com>
2023-08-15 13:28:58 +0200

    Put CodeChecker invocations directly into the responsible job (instead of separate bash script)

--- ci/codechecker.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Stolen from @afust and modified
-# see: https://git.gams.com/devel/gams-mii/-/blob/develop/ci/codechecker.sh
-# Log your project.
-CodeChecker log -b "make -j4" -o compilation_database.json
-CodeChecker analyze --analyzers clang-tidy clangsa -o reports compilation_database.json
-# Create the report file by using the CodeChecker parse command.
-CodeChecker parse --trim-path-prefix $(pwd) -e codeclimate reports > gl-code-quality-report.json
-exit 0
