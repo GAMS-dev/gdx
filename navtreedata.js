8536f50b7627600135fd89b13c379cda09dcd354
0x17 <0x17@users.noreply.github.com>
2023-10-27 07:41:56 +0000

    deploy: 4c1475008364e4de7e3cbb8ca94bb18fa4a81c47

--- navtreedata.js
+++ navtreedata.js
@@ -26,45 +26,46 @@ var NAVTREE =
 [
   [ "GAMS Data eXchange (GDX) API", "index.html", [
     [ "GAMS Data eXchange (GDX)", "index.html", [
-      [ "Basic information on GDX file format", "index.html#autotoc_md1", [
-        [ "Information contained inside a GDX file", "index.html#autotoc_md2", null ],
-        [ "Features of the GDX API", "index.html#autotoc_md3", null ]
+      [ "Table of Contents", "index.html#autotoc_md1", null ],
+      [ "Basic information on GDX file format", "index.html#autotoc_md2", [
+        [ "Information contained inside a GDX file", "index.html#autotoc_md3", null ],
+        [ "Features of the GDX API", "index.html#autotoc_md4", null ]
       ] ],
-      [ "Setting up and building GDX", "index.html#autotoc_md4", [
-        [ "Accessing GDX from a custom application", "index.html#autotoc_md5", null ],
-        [ "Building GDX from source", "index.html#autotoc_md6", null ]
+      [ "Setting up and building GDX", "index.html#autotoc_md5", [
+        [ "Accessing GDX from a custom application", "index.html#autotoc_md6", null ],
+        [ "Building GDX from source", "index.html#autotoc_md7", null ]
       ] ],
-      [ "Reference documentation", "index.html#autotoc_md7", null ],
-      [ "Introduction into using GDX API", "index.html#autotoc_md8", [
-        [ "Writing data to a GDX file", "index.html#autotoc_md9", null ],
-        [ "Writing data using strings", "index.html#autotoc_md10", null ],
-        [ "Writing data using integers (raw)", "index.html#autotoc_md11", null ],
-        [ "Writing data using integers (mapped)", "index.html#autotoc_md12", null ],
-        [ "Reading data from a GDX file", "index.html#autotoc_md13", null ],
-        [ "Reading data using strings", "index.html#autotoc_md14", null ],
-        [ "Reading data using integers (raw)", "index.html#autotoc_md15", null ],
-        [ "Reading data using integers (Mapped)", "index.html#autotoc_md16", null ],
-        [ "Reading data using a filter", "index.html#autotoc_md17", null ],
-        [ "Dealing with acronyms", "index.html#autotoc_md18", null ]
+      [ "Reference documentation", "index.html#autotoc_md8", null ],
+      [ "Introduction into using GDX API", "index.html#autotoc_md9", [
+        [ "Writing data to a GDX file", "index.html#autotoc_md10", null ],
+        [ "Writing data using strings", "index.html#autotoc_md11", null ],
+        [ "Writing data using integers (raw)", "index.html#autotoc_md12", null ],
+        [ "Writing data using integers (mapped)", "index.html#autotoc_md13", null ],
+        [ "Reading data from a GDX file", "index.html#autotoc_md14", null ],
+        [ "Reading data using strings", "index.html#autotoc_md15", null ],
+        [ "Reading data using integers (raw)", "index.html#autotoc_md16", null ],
+        [ "Reading data using integers (Mapped)", "index.html#autotoc_md17", null ],
+        [ "Reading data using a filter", "index.html#autotoc_md18", null ],
+        [ "Dealing with acronyms", "index.html#autotoc_md19", null ]
       ] ],
-      [ "Functions by Category", "index.html#autotoc_md19", null ],
-      [ "Transition diagram", "index.html#autotoc_md20", [
-        [ "Diagram for general operations", "index.html#autotoc_md21", null ],
-        [ "Diagram for read operations", "index.html#autotoc_md22", null ],
-        [ "Diagram for write operations", "index.html#autotoc_md23", null ],
-        [ "Diagram for UEL operations", "index.html#autotoc_md24", null ]
+      [ "Functions by Category", "index.html#autotoc_md20", null ],
+      [ "Transition diagram", "index.html#autotoc_md21", [
+        [ "Diagram for general operations", "index.html#autotoc_md22", null ],
+        [ "Diagram for read operations", "index.html#autotoc_md23", null ],
+        [ "Diagram for write operations", "index.html#autotoc_md24", null ],
+        [ "Diagram for UEL operations", "index.html#autotoc_md25", null ]
       ] ],
-      [ "Example programs", "index.html#autotoc_md25", [
-        [ "Example 1 (GAMS)", "index.html#autotoc_md26", null ],
-        [ "Example 2 (C)", "index.html#autotoc_md27", null ],
-        [ "Example 3 (C++)", "index.html#autotoc_md28", null ],
-        [ "Example 4 (VB.NET)", "index.html#autotoc_md29", null ],
-        [ "Example 6 (Python)", "index.html#autotoc_md30", null ],
-        [ "Example 7 (C#)", "index.html#autotoc_md31", null ],
-        [ "Example 8 (Java)", "index.html#autotoc_md32", null ]
+      [ "Example programs", "index.html#autotoc_md26", [
+        [ "Example 1 (GAMS)", "index.html#autotoc_md27", null ],
+        [ "Example 2 (C)", "index.html#autotoc_md28", null ],
+        [ "Example 3 (C++)", "index.html#autotoc_md29", null ],
+        [ "Example 4 (VB.NET)", "index.html#autotoc_md30", null ],
+        [ "Example 6 (Python)", "index.html#autotoc_md31", null ],
+        [ "Example 7 (C#)", "index.html#autotoc_md32", null ],
+        [ "Example 8 (Java)", "index.html#autotoc_md33", null ]
       ] ],
-      [ "Conversion issues when moving from GAMS 22.5 to 22.6", "index.html#autotoc_md33", null ],
-      [ "Files in the apifiles directory", "index.html#autotoc_md34", null ]
+      [ "Conversion issues when moving from GAMS 22.5 to 22.6", "index.html#autotoc_md34", null ],
+      [ "Files in the apifiles directory", "index.html#autotoc_md35", null ]
     ] ],
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
