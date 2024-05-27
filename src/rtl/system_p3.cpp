e22b7b70f0f32c508435e57910e43eb94171297d
André Schnabel <aschnabel@gams.com>
2024-05-27 17:34:04 +0200

    Added system_p3::getdir

---
+++ src/rtl/system_p3.cpp
@@ -0,0 +1,37 @@
+#include "system_p3.h"
+
+#if defined(_WIN32)
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
+namespace rtl::system_p3 {
+
+void getdir(uint8_t d, std::string &s)
+{
+#if defined(_WIN32)
+   char Drive[3], DirBuf[MAX_PATH], SaveBuf[MAX_PATH];
+   if (d) {
+      Drive[0] = 'A' + d - 1;
+      Drive[1] = ':';
+      Drive[2] = '\0';
+      GetCurrentDirectoryA(sizeof(SaveBuf), SaveBuf);
+      SetCurrentDirectoryA(Drive);
+   }
+   GetCurrentDirectoryA(sizeof(DirBuf), DirBuf);
+   if (d) SetCurrentDirectoryA(SaveBuf);
+   s.assign(DirBuf);
+#else
+   char DirBuf[512];
+   if( const char *p { getcwd( DirBuf, sizeof( DirBuf ) ) }; !p )
+   {
+      // this is unlikely, and not handled well here: exceptions?
+      s.clear();
+      return;
+   }
+   s.assign( DirBuf );
+#endif
+}
+
+}
