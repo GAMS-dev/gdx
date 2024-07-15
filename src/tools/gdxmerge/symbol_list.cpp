66501ce48576c953030fd2eec209bc05b8058354
Michael Horstmann <git@hrstmnn.de>
2024-07-15 17:54:45 +0200

    added empty symbol list class

---
+++ src/tools/gdxmerge/symbol_list.cpp
@@ -0,0 +1,8 @@
+#include "symbol_list.h"
+
+namespace gdxmerge
+{
+
+symbol_list::symbol_list() = default;
+
+}// namespace gdxmerge
---
+++ src/tools/gdxmerge/symbol_list.h
@@ -0,0 +1,20 @@
+#ifndef GDX_SYMBOL_LIST_H
+#define GDX_SYMBOL_LIST_H
+
+#include <string>
+
+namespace gdxmerge
+{
+
+class symbol_list
+{
+private:
+   std::string attribute;
+
+public:
+   symbol_list();
+};
+
+}// namespace gdxmerge
+
+#endif//GDX_SYMBOL_LIST_H
