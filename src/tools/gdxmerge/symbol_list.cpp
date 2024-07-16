b21cdfccd7ecd7d5b66dc0b8c879f4c4ba3fef25
Michael Horstmann <git@hrstmnn.de>
2024-07-16 15:15:24 +0200

    removed unnecessary code

--- src/tools/gdxmerge/symbol_list.cpp
+++ src/tools/gdxmerge/symbol_list.cpp
@@ -6,10 +6,7 @@ namespace gdxmerge
 template<typename T>
 TGAMSSymbol<T>::TGAMSSymbol( const int ADim, const int AType, const int ASubTyp ) : syDim( ADim ), syTyp( AType ), sySubTyp( ASubTyp )
 {
-   // syExplTxt.clear();
    syData = new gdlib::gmsdata::TTblGamsData<T>( ADim, sizeof( T ) );
-   // sySize = 0;
-   // syMemory = 0;
    sySkip = false;
 };
 
