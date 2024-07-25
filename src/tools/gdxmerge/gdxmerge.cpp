3a997cf12a7b8377ce670a2649d0bc8c693939ae
Michael Horstmann <git@hrstmnn.de>
2024-07-25 09:23:49 +0200

    improved TSymbolList constructor

--- src/tools/gdxmerge/gdxmerge.cpp
+++ src/tools/gdxmerge/gdxmerge.cpp
@@ -102,7 +102,7 @@ TSymbolList<T>::TSymbolList()
    StrPool = new gdlib::gmsobj::TXStrPool<T>();
    const std::string empty_string;
    StrPool->Add( empty_string.data(), empty_string.length() );
-   FileList = new TFileList<T>;
+   FileList = new TFileList<T>();
    library::short_string Msg;
    gdxCreate( &PGXMerge, Msg.data(), Msg.length() );
 }
