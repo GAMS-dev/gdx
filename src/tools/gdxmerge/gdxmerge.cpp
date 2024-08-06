7140f4ea8fabedbd630ed4fac7a43acd0c478e82
Michael Horstmann <git@hrstmnn.de>
2024-08-06 15:08:23 +0200

    fixed FindGDXFiles function

--- src/tools/gdxmerge/gdxmerge.cpp
+++ src/tools/gdxmerge/gdxmerge.cpp
@@ -354,7 +354,7 @@ bool TSymbolList::FindGDXFiles( const std::string &Path )
 {
    rtl::sysutils_p3::TSearchRec Rec {};
    std::string WPath, BPath, ShortName, NewName, DTS;
-   std::tm DT {};
+   double DT;
 
    // Normal file or file pattern
    bool Result { true };
@@ -383,19 +383,13 @@ bool TSymbolList::FindGDXFiles( const std::string &Path )
             ShortName = NewName;
          }
 
-         // TODO: Check whether this works
+         DT = rtl::sysutils_p3::FileDateToDateTime( Rec.Time );
+
          uint16_t Year, Month, Day, Hour, Min, Sec, MSec;
-         rtl::sysutils_p3::DecodeDate( Rec.Time, Year, Month, Day );
-         rtl::sysutils_p3::DecodeTime( Rec.Time, Hour, Min, Sec, MSec );
+         rtl::sysutils_p3::DecodeDate( DT, Year, Month, Day );
+         rtl::sysutils_p3::DecodeTime( DT, Hour, Min, Sec, MSec );
 
-         DT.tm_year = Year - 1900;
-         DT.tm_mon = Month - 1;
-         DT.tm_mday = Day;
-         DT.tm_hour = Hour;
-         DT.tm_min = Min;
-         DT.tm_sec = Sec;
-
-         DTS = FormatDateTime( DT );
+         DTS = FormatDateTime( Year, Month, Day, Hour, Min, Sec );
          FileList->AddFile( BPath + Rec.Name, ShortName, DTS + "  " + BPath + Rec.Name );
       } while( FindNext( Rec ) == 0 );
       FindClose( Rec );
