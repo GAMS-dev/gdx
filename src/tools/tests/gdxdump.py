25f9e30e81f500f0811d82d9b6127f6120e7e64c
Michael Horstmann <git@hrstmnn.de>
2024-08-19 15:23:40 +0200

    added gdxdump tests

--- src/tools/tests/gdxdump.py
+++ src/tools/tests/gdxdump.py
@@ -1,15 +1,38 @@
 import unittest
-import sys
-import gams
+import os
+import subprocess
+from examples.small_example import create_small_example
+from examples.full_example import create_full_example
+
+
+def run_gdxdump(command: list[str]) -> subprocess.CompletedProcess[str]:
+    return subprocess.run(
+        [os.path.join('..', '..', '..', 'build', 'gdxdump'), *command],
+        capture_output=True,
+        text=True
+    )
 
 
 class TestGdxDump(unittest.TestCase):
 
-    def test_gams_module(self):
-        self.assertTrue('gams' in sys.modules)
+    def setUp(self):
+        create_small_example(os.path.join('examples', 'small_example.gdx'))
+        create_full_example(os.path.join('examples', 'full_example.gdx'))
 
-    def test_gams_api(self):
-        self.assertRegex(
-            f'API OK -- Version {gams.__version__}',
-            r'API OK -- Version [0-9]+\.[0-9]+\.[0-9]+'
-        )
+    def tearDown(self):
+        os.remove(os.path.join('examples', 'small_example.gdx'))
+        os.remove(os.path.join('examples', 'full_example.gdx'))
+
+    def test_small_example(self):
+        output = run_gdxdump([os.path.join('examples', 'small_example.gdx')])
+        self.assertEqual(output.returncode, 0)
+        with open(os.path.join('output', 'gdxdump', 'small_example.txt'), 'r') as file:
+            self.assertEqual(output.stdout, file.read())
+        self.assertEqual(output.stderr, '')
+
+    def test_full_example(self):
+        output = run_gdxdump([os.path.join('examples', 'full_example.gdx')])
+        self.assertEqual(output.returncode, 0)
+        with open(os.path.join('output', 'gdxdump', 'full_example.txt'), 'r') as file:
+            self.assertEqual(output.stdout, file.read())
+        self.assertEqual(output.stderr, '')
