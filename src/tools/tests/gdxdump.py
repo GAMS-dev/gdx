458b8e012e88076a84960df0684c84f9641c3f43
Michael Horstmann <git@hrstmnn.de>
2024-08-20 15:20:58 +0200

    added another gdxdump test

--- src/tools/tests/gdxdump.py
+++ src/tools/tests/gdxdump.py
@@ -241,3 +241,16 @@ class TestGdxDump(unittest.TestCase):
             second = file.read().split('\n')
         self.assertEqual(first, second)
         self.assertEqual(output.stderr, '')
+
+    def test_full_example_missing_delimiter(self) -> None:
+        output = run_gdxdump([
+            self.FULL_EXAMPLE_FILE_PATH,
+            'Delim'
+        ])
+        self.assertEqual(output.returncode, 1)
+        first = output.stdout.split('\n')
+        with open(os.path.join(self.OUTPUT_DIRECTORY_PATH, 'full_example_missing_delimiter.txt'), 'r') as file:
+            second = file.read().split('\n')
+        del second[2]
+        self.assertEqual(first, second)
+        self.assertEqual(output.stderr, '')
