97bdecfc3d279086796f9f9d6eee1fcd12e14a1c
Michael Horstmann <git@hrstmnn.de>
2024-08-20 11:11:20 +0200

    upgrade pip in install script

--- src/tools/tests/environments/venv/install.sh
+++ src/tools/tests/environments/venv/install.sh
@@ -4,4 +4,5 @@ DIRECTORY_PATH="$(dirname $0)"
 
 python3 -m venv "$DIRECTORY_PATH/../../venv"
 source "$DIRECTORY_PATH/../../venv/bin/activate"
+python3 -m pip install --upgrade pip
 python3 -m pip install -r "$DIRECTORY_PATH/requirements.txt"
