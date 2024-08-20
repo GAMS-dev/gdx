6ac093ee7d57ff9dacffe97c439fe65784e7546d
Michael Horstmann <git@hrstmnn.de>
2024-08-20 18:23:55 +0200

    added more gdxdump tests

--- src/tools/tests/gdxdump.py
+++ src/tools/tests/gdxdump.py
@@ -302,3 +302,28 @@ class TestGdxDump(unittest.TestCase):
             second = file.read().split('\n')
         self.assertEqual(first, second)
         self.assertEqual(output.stderr, '')
+
+    def test_full_example_format_csv(self) -> None:
+        output = run_gdxdump([
+            self.FULL_EXAMPLE_FILE_PATH,
+            'Format=csv'
+        ])
+        self.assertEqual(output.returncode, 1)
+        first = output.stdout.split('\n')
+        with open(os.path.join(self.OUTPUT_DIRECTORY_PATH, 'full_example_format_csv.txt'), 'r') as file:
+            second = file.read().split('\n')
+        self.assertEqual(first, second)
+        self.assertEqual(output.stderr, '')
+
+    def test_full_example_symbol_format_csv(self) -> None:
+        output = run_gdxdump([
+            self.FULL_EXAMPLE_FILE_PATH,
+            'Symb=a',
+            'Format=csv'
+        ])
+        self.assertEqual(output.returncode, 0)
+        first = output.stdout.split('\n')
+        with open(os.path.join(self.OUTPUT_DIRECTORY_PATH, 'full_example_symbol_format_csv.txt'), 'r') as file:
+            second = file.read().split('\n')
+        self.assertEqual(first, second)
+        self.assertEqual(output.stderr, '')
