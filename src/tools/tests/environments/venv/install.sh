2ebde921426371df0a7a517f7dada80736c37b94
Michael Horstmann <git@hrstmnn.de>
2024-08-20 11:11:59 +0200

    added empty line

--- src/tools/tests/environments/venv/install.sh
+++ src/tools/tests/environments/venv/install.sh
@@ -4,5 +4,6 @@ DIRECTORY_PATH="$(dirname $0)"
 
 python3 -m venv "$DIRECTORY_PATH/../../venv"
 source "$DIRECTORY_PATH/../../venv/bin/activate"
+
 python3 -m pip install --upgrade pip
 python3 -m pip install -r "$DIRECTORY_PATH/requirements.txt"
