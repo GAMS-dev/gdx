351358becdc330f0abab4b2295b6c310cf71b028
Michael Horstmann <git@hrstmnn.de>
2024-08-21 14:43:14 +0200

    added another gdxdump test

--- src/tools/tests/gdxdump.py
+++ src/tools/tests/gdxdump.py
@@ -522,3 +522,17 @@ class TestGdxDump(unittest.TestCase):
             second = file.read().split('\n')
         self.assertEqual(first, second)
         self.assertEqual(output.stderr, '')
+
+    def test_full_example_symbol_format_csv_cdim(self) -> None:
+        output = run_gdxdump([
+            self.FULL_EXAMPLE_FILE_PATH,
+            'Symb=x',
+            'Format=csv',
+            'cDim=Y'
+        ])
+        self.assertEqual(output.returncode, 0)
+        first = output.stdout.split('\n')
+        with open(os.path.join(self.OUTPUT_DIRECTORY_PATH, 'full_example_symbol_format_csv_cdim.txt'), 'r') as file:
+            second = file.read().split('\n')
+        self.assertEqual(first, second)
+        self.assertEqual(output.stderr, '')
