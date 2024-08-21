879fab06d18c6c5dcd73464f6344b114c943952b
Michael Horstmann <git@hrstmnn.de>
2024-08-21 14:10:27 +0200

    added another gdxdump test

--- src/tools/tests/gdxdump.py
+++ src/tools/tests/gdxdump.py
@@ -486,3 +486,15 @@ class TestGdxDump(unittest.TestCase):
             second = file.read().split('\n')
         self.assertEqual(first, second)
         self.assertEqual(output.stderr, '')
+
+    def test_full_example_symbols_as_set_domain_information(self) -> None:
+        output = run_gdxdump([
+            self.FULL_EXAMPLE_FILE_PATH,
+            'SymbolsAsSetDI'
+        ])
+        self.assertEqual(output.returncode, 0)
+        first = output.stdout.split('\n')
+        with open(os.path.join(self.OUTPUT_DIRECTORY_PATH, 'full_example_symbols_as_set_domain_information.txt'), 'r') as file:
+            second = file.read().split('\n')
+        self.assertEqual(first, second)
+        self.assertEqual(output.stderr, '')
