7e5ee95dbc5f562a764caf211a1b5e6c4179e322
Michael Horstmann <git@hrstmnn.de>
2024-08-21 10:53:56 +0200

    added lowercase version of test

--- src/tools/tests/gdxdump.py
+++ src/tools/tests/gdxdump.py
@@ -391,3 +391,15 @@ class TestGdxDump(unittest.TestCase):
             second = file.read().split('\n')
         self.assertEqual(first, second)
         self.assertEqual(output.stderr, '')
+
+    def test_full_example_numerical_format_hexbytes_lowercase(self) -> None:
+        output = run_gdxdump([
+            self.FULL_EXAMPLE_FILE_PATH,
+            'dformat=hexbytes'
+        ])
+        self.assertEqual(output.returncode, 0)
+        first = output.stdout.split('\n')
+        with open(os.path.join(self.OUTPUT_DIRECTORY_PATH, 'full_example_numerical_format_hexbytes.txt'), 'r') as file:
+            second = file.read().split('\n')
+        self.assertEqual(first, second)
+        self.assertEqual(output.stderr, '')
