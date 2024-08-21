aa3ad90647b1ab64a7abb78effa9dea6e4085945
Michael Horstmann <git@hrstmnn.de>
2024-08-21 14:52:23 +0200

    added another gdxdump test

--- src/tools/tests/gdxdump.py
+++ src/tools/tests/gdxdump.py
@@ -536,3 +536,15 @@ class TestGdxDump(unittest.TestCase):
             second = file.read().split('\n')
         self.assertEqual(first, second)
         self.assertEqual(output.stderr, '')
+
+    def test_full_example_filter_default_values(self) -> None:
+        output = run_gdxdump([
+            self.FULL_EXAMPLE_FILE_PATH,
+            'FilterDef=N'
+        ])
+        self.assertEqual(output.returncode, 0)
+        first = output.stdout.split('\n')
+        with open(os.path.join(self.OUTPUT_DIRECTORY_PATH, 'full_example_filter_default_values.txt'), 'r') as file:
+            second = file.read().split('\n')
+        self.assertEqual(first, second)
+        self.assertEqual(output.stderr, '')
