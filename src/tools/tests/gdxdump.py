0ba9bdc2ed4380336cfbaf6a9834ea97dcffa8e0
Michael Horstmann <git@hrstmnn.de>
2024-08-21 10:59:32 +0200

    added another gdxdump test

--- src/tools/tests/gdxdump.py
+++ src/tools/tests/gdxdump.py
@@ -342,6 +342,20 @@ class TestGdxDump(unittest.TestCase):
         self.assertEqual(first, second)
         self.assertEqual(output.stderr, '')
 
+    def test_full_example_symbol_format_csv_header_missing_identifier(self) -> None:
+        output = run_gdxdump([
+            self.FULL_EXAMPLE_FILE_PATH,
+            'Symb=a',
+            'Format=csv',
+            'Header'
+        ])
+        self.assertEqual(output.returncode, 0)
+        first = output.stdout.split('\n')
+        with open(os.path.join(self.OUTPUT_DIRECTORY_PATH, 'full_example_symbol_format_csv_header_missing_identifier.txt'), 'r') as file:
+            second = file.read().split('\n')
+        self.assertEqual(first, second)
+        self.assertEqual(output.stderr, '')
+
     def test_full_example_symbol_format_csv_no_header(self) -> None:
         output = run_gdxdump([
             self.FULL_EXAMPLE_FILE_PATH,
