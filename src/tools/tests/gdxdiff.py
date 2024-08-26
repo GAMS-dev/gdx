178f5a69fcb809559e6c62665177b3ae31d6659a
Michael Horstmann <git@hrstmnn.de>
2024-08-26 11:12:18 +0200

    improved gdxdiff test

--- src/tools/tests/gdxdiff.py
+++ src/tools/tests/gdxdiff.py
@@ -614,3 +614,19 @@ class TestGdxDiff(unittest.TestCase):
             ]
         }
         self.check_gdx_file(symbols)
+
+        output_space_separator = self.run_gdxdiff([
+            self.FILE_PATHS['full_example'],
+            self.FILE_PATHS['full_example_changed_data_and_variables'],
+            self.FILE_PATHS['diff_file'],
+            'Id="c d"'
+        ])
+        self.check_output(
+            output_space_separator,
+            return_code=1,
+            first_offset=2,
+            second_offset=3,
+            first_delete=[-3],
+            second_delete=[-3]
+        )
+        self.check_gdx_file(symbols)
