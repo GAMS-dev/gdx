83db1232b1955b0cc0def190153edaa1d7dcb1d1
Michael Horstmann <git@hrstmnn.de>
2024-08-27 10:03:05 +0200

    added label example file

---
+++ src/tools/tests/examples/label_example.py
@@ -0,0 +1,49 @@
+import gams.transfer as gt
+import pandas as pd
+
+
+def get_test_string(count=255) -> str:
+    return f'|{'_' * (count - 2)}|'
+
+
+def create_label_example(file_path: str) -> None:
+    m = gt.Container()
+
+    # create the set i
+    i = gt.Set(m, 'i', records=[
+        ('seattle', get_test_string()),
+        ('san-diego', get_test_string())
+    ], description=get_test_string())
+
+    # create the set j
+    j = gt.Set(m, 'j', records=[
+        ('new-york', get_test_string()),
+        ('chicago', get_test_string()),
+        ('topeka', get_test_string())
+    ], description=get_test_string())
+
+    # add 'd' parameter -- domain linked to set objects i and j
+    d = gt.Parameter(m, 'd', [i, j], description=get_test_string())
+
+    # create some data as a generic DataFrame
+    dist = pd.DataFrame(
+        [
+            ('seattle', 'new-york', 2.5),
+            ('seattle', 'chicago', 1.7),
+            ('seattle', 'topeka', 1.8),
+            ('san-diego', 'new-york', 2.5),
+            ('san-diego', 'chicago', 1.8),
+            ('san-diego', 'topeka', 1.4),
+        ],
+        columns=[
+            get_test_string(),
+            get_test_string(),
+            get_test_string()
+        ]
+    )
+
+    # setRecords will automatically convert the dist DataFrame into a standard DataFrame format
+    d.setRecords(dist)
+
+    # write the GDX
+    m.write(file_path)
