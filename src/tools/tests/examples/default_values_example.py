1b33ccc1779439025079de9c839c91464cb02db6
Michael Horstmann <git@hrstmnn.de>
2024-08-28 10:19:21 +0200

    added default values examples file

---
+++ src/tools/tests/examples/default_values_example.py
@@ -0,0 +1,37 @@
+import gams.transfer as gt
+import pandas as pd
+
+
+def create_default_values_example_1(file_path: str) -> None:
+    m = gt.Container()
+
+    v = gt.Variable(
+        m,
+        'v',
+        'free',
+        domain=['*'],
+        records=pd.DataFrame(
+            data=[('i' + str(i), i) for i in range(5)],
+            columns=['domain', 'marginal']
+        )
+    )
+
+    m.write(file_path)
+
+
+def create_default_values_example_2(file_path: str) -> None:
+    m = gt.Container()
+
+    v = gt.Variable(
+        m,
+        'v',
+        'free',
+        domain=['*'],
+        records=pd.DataFrame(
+            data=[('i' + str(i), i) for i in range(5)],
+            columns=['domain', 'marginal']
+        )
+    )
+    v.records.drop(0, inplace=True)
+
+    m.write(file_path)
