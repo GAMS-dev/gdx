fee02ebfc7e038892437cc8ec0bbe283b34d8946
Michael Horstmann <git@hrstmnn.de>
2024-09-24 17:52:18 +0200

    added new classes to gdx2veda header file

--- src/tools/gdx2veda/gdx2veda.h
+++ src/tools/gdx2veda/gdx2veda.h
@@ -16,6 +16,46 @@
 namespace gdx2veda
 {
 
+class TMyHashList
+{
+public:
+   int Compare( const library::short_string &PS1, const library::short_string &PS2 );
+};
+
+class THashTuple
+{
+private:
+   TMyHashList StringStore;
+
+   library::short_string Encode( int x, int n );
+   void Decode( const library::short_string &s, int x, int n );
+
+public:
+   THashTuple();
+   void Add( int x, int n, int mark );
+   int Find( int x, int n, int &mark );
+   int GetCount();
+   int Get( int i, int &n );
+   int GetSorted( int i, int &n );
+   void SetText( int i, int t );
+   int GetText( int i );
+   int GetSortedText( int i );
+   void ClearText();
+};
+
+class TProjection
+{
+private:
+   library::one_indexed_container<THashTuple> tabs { GMS_MAX_INDEX_DIM };
+
+public:
+   TProjection();
+   int AddUel( int dimension, int x, int n, int &mark );
+   int AddInteger( int dimension, int k );
+   int GetUel( int dimension, int i, int &n );
+   int GetSortedUel( int dimension, int i, int &n );
+};
+
 std::string StripExt( const std::string &fln );
 
 std::string DQuotedStr( const std::string &s );
