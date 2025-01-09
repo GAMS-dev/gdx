69078c7f1e3609510845e9687888590cb025d29f
0x17 <0x17@users.noreply.github.com>
2025-01-09 08:47:48 +0000

    deploy: 01d18fe10ab5687436813357adf2820058b5cfcc

--- dir_68267d1309a1af8e8297ef4c3efbcdba.js
+++ dir_68267d1309a1af8e8297ef4c3efbcdba.js
@@ -1,5 +1,5 @@
 var dir_68267d1309a1af8e8297ef4c3efbcdba =
 [
-    [ "gdx.h", "gdx_8h_source.html", null ],
-    [ "gxfile.h", "gxfile_8h_source.html", null ]
+    [ "gdx.hpp", "gdx_8hpp_source.html", null ],
+    [ "gxfile.hpp", "gxfile_8hpp_source.html", null ]
 ];
