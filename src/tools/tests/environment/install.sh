0914640396510945c6df6e21a71c2768f4ff0c94
Michael Horstmann <mhorstmann@gams.com>
2025-02-06 16:22:01 +0100

    removed conda environment

--- src/tools/tests/environment/install.sh
+++ src/tools/tests/environment/install.sh
@@ -2,8 +2,8 @@
 
 DIRECTORY_PATH="$(dirname $0)"
 
-python3 -m venv "$DIRECTORY_PATH/../../venv"
-source "$DIRECTORY_PATH/../../venv/bin/activate"
+python3 -m venv "$DIRECTORY_PATH/../venv"
+source "$DIRECTORY_PATH/../venv/bin/activate"
 
 python3 -m pip install --upgrade pip
 python3 -m pip install -r "$DIRECTORY_PATH/requirements.txt"
