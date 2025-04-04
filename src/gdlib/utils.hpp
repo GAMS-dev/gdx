4485d76cac5cc668d054328bdbfb29f348b9ab19
André Schnabel <aschnabel@gams.com>
2025-04-04 16:40:40 +0200

    Make implicit sstring to char * cast constexpr

--- src/gdlib/utils.hpp
+++ src/gdlib/utils.hpp
@@ -798,8 +798,8 @@ auto ui32(const T x)
 class sstring : public std::array<char, 256> {
    public:
       std::string str() const { return { data() }; }
-      const char *c_str() const { return data(); }
-      operator char *() { return data();  }
+      constexpr const char *c_str() const noexcept { return data(); }
+      constexpr operator char *() noexcept { return data();  }
       size_t len() const { return std::strlen( data() ); }
       char last() const { return ( *this )[len() - 1];  }
       template<typename T>
