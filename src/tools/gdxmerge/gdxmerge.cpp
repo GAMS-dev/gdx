cf008d83a8e5d9f17b6e190d81e1681e4aa687ca
Michael Horstmann <mhorstmann@gams.com>
2025-07-31 14:17:44 +0200

    improve usage procedures

--- src/tools/gdxmerge/gdxmerge.cpp
+++ src/tools/gdxmerge/gdxmerge.cpp
@@ -657,18 +657,19 @@ bool GetParameters(const int argc, const char *argv[]) {
 }
 
 void Usage(const library::AuditLine &auditLine) {
-  std::cout << "gdxmerge: Merge GDX files\n"
-            << auditLine.getAuditLine() << "\n\n"
-            << "Usage:\n"
-            << "   gdxmerge filepat1 filepat2 ... filepatn\n"
-            << "     Optional parameters:\n"
-            << "          id=ident1, ident2: Merge specified IDs only\n"
-            << "          exclude=ident1, ident2: Do not merge specified IDs\n"
-            << "          big=<integer>    : Size indicator for a large symbol\n"
-            << "          output=filename  : Output file; merged.gdx by default\n"
-            << "          strict=true/false: Terminate on failure, e.g. missing input files\n"
-            << "Filepat represents a filename or a file pattern.\n"
-            << "The form: @filename will process parameters from that file" << std::endl;
+  std::cout
+      << "gdxmerge: Merge GDX files\n"
+      << auditLine.getAuditLine()
+      << "\n\nUsage:\n"
+         "   gdxmerge filepat1 filepat2 ... filepatn\n"
+         "     Optional parameters:\n"
+         "          id=ident1, ident2: Merge specified IDs only\n"
+         "          exclude=ident1, ident2: Do not merge specified IDs\n"
+         "          big=<integer>    : Size indicator for a large symbol\n"
+         "          output=filename  : Output file; merged.gdx by default\n"
+         "          strict=true/false: Terminate on failure, e.g. missing input files\n"
+         "Filepat represents a filename or a file pattern.\n"
+         "The form: @filename will process parameters from that file\n";
 }
 
 int main(const int argc, const char *argv[]) {
