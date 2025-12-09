c35b11954c2f9ccc96246fff65bae67862f4aabc
André Schnabel <aschnabel@gams.com>
2025-12-09 16:36:13 +0100

    Invert rc for mkdir

--- src/rtl/p3io.cpp
+++ src/rtl/p3io.cpp
@@ -480,13 +480,13 @@ void P3WriteFS(P3File *fil, const char *s)
 /**
  * Create a directory
  * @param s name of the directory
- * @return true on success and false on failure
+ * @return 0 on success and !=0 on failure
  */
 bool mkdir(const std::string& s) {
 #if defined(_WIN32)
-   return !_mkdir( s.c_str() );
+   return _mkdir( s.c_str() );
 #else
-   return !::mkdir( s.c_str(), 0777 );
+   return ::mkdir( s.c_str(), 0777 );
 #endif
 }
 
