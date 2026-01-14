efec3f9e39dcf038d3542f394c39ba87d6303fb9
André Schnabel <aschnabel@gams.com>
2026-01-14 14:58:24 +0100

    Updated copyright notices to 2026

--- src/gdlib/strutilx.hpp
+++ src/gdlib/strutilx.hpp
@@ -1,8 +1,8 @@
 /*
 * GAMS - General Algebraic Modeling System GDX API
  *
- * Copyright (c) 2017-2025 GAMS Software GmbH <support@gams.com>
- * Copyright (c) 2017-2025 GAMS Development Corp. <support@gams.com>
+ * Copyright (c) 2017-2026 GAMS Software GmbH <support@gams.com>
+ * Copyright (c) 2017-2026 GAMS Development Corp. <support@gams.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
