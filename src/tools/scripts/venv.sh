92a3401ab1fd6576df5b7d0379c9d488d5b7ae64
Michael Horstmann <mhorstmann@gams.com>
2026-01-16 17:36:20 +0100

    Don't install dev packages by default

--- src/tools/scripts/venv.sh
+++ src/tools/scripts/venv.sh
@@ -5,4 +5,4 @@ set -e
 source "$(dirname "$0")/directories.sh"
 
 export UV_PROJECT_ENVIRONMENT="$VENV_DIRECTORY"
-uv sync --project "$TESTS_DIRECTORY"
+uv sync --project "$TESTS_DIRECTORY" --no-dev
