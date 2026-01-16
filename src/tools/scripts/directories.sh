c684a35a984c90b7db4aab1ae1adc9a13688c6ee
Michael Horstmann <mhorstmann@gams.com>
2026-01-16 17:12:51 +0100

    Add script to create venv with uv

--- src/tools/scripts/directories.sh
+++ src/tools/scripts/directories.sh
@@ -4,4 +4,6 @@ set -e
 
 SCRIPTS_DIRECTORY="$(realpath "$(dirname "$0")")"
 TOOLS_DIRECTORY="$(realpath "$SCRIPTS_DIRECTORY/..")"
+TESTS_DIRECTORY="$TOOLS_DIRECTORY/tests"
 GDX_DIRECTORY="$(realpath "$TOOLS_DIRECTORY/../..")"
+VENV_DIRECTORY="$GDX_DIRECTORY/.venv"
---
+++ src/tools/scripts/venv.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+source "$(dirname "$0")/directories.sh"
+
+export UV_PROJECT_ENVIRONMENT="$VENV_DIRECTORY"
+uv sync --project "$TESTS_DIRECTORY"
