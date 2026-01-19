4430e30e915e38caaaa078d2ad79716d7af0f5b9
Michael Horstmann <mhorstmann@gams.com>
2026-01-19 17:44:32 +0100

    Move uv files to tools directory

--- src/tools/scripts/directories.sh
+++ src/tools/scripts/directories.sh
@@ -4,6 +4,5 @@ set -e
 
 SCRIPTS_DIRECTORY="$(realpath "$(dirname "$0")")"
 TOOLS_DIRECTORY="$(realpath "$SCRIPTS_DIRECTORY/..")"
-TESTS_DIRECTORY="$TOOLS_DIRECTORY/tests"
 GDX_DIRECTORY="$(realpath "$TOOLS_DIRECTORY/../..")"
 VENV_DIRECTORY="$GDX_DIRECTORY/.venv"
--- src/tools/scripts/venv.sh
+++ src/tools/scripts/venv.sh
@@ -5,4 +5,4 @@ set -e
 source "$(dirname "$0")/directories.sh"
 
 export UV_PROJECT_ENVIRONMENT="$VENV_DIRECTORY"
-uv sync --project "$TESTS_DIRECTORY" --no-dev
+uv sync --project "$TOOLS_DIRECTORY" --no-dev
