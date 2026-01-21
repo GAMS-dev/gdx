9a06efdf5be0d1dceb9a2da720f082b4cc43735e
Michael Horstmann <mhorstmann@gams.com>
2026-01-21 18:08:36 +0100

    Also install dev packages for IDE

--- src/tools/scripts/venv.sh
+++ src/tools/scripts/venv.sh
@@ -6,4 +6,4 @@ set -e
 source "$(dirname "$0")/directories.sh"
 
 export UV_PROJECT_ENVIRONMENT="$VENV_DIRECTORY"
-uv sync --directory "$TOOLS_DIRECTORY" --no-dev
+uv sync --directory "$TOOLS_DIRECTORY"
