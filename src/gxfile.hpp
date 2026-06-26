eb19d7d1fe6859fbeac7f41b6728bdf60c807d1d
Olivier Huber <ohuber@gams.com>
2026-06-26 13:56:00 +0200

    Apply suggestion by @schnabel

--- src/gxfile.hpp
+++ src/gxfile.hpp
@@ -262,7 +262,6 @@ class TUELTable final : public TXStrHashListImpl<int>
 public:
    std::unique_ptr<TIntegerMapping> UsrUel2Ent {};// from user uelnr to table entry
    TUELTable();
-   //~TUELTable() override = default;
    [[nodiscard]] int size() const;
    [[nodiscard]] bool empty() const;
    [[nodiscard]] int GetUserMap( int i ) const;
